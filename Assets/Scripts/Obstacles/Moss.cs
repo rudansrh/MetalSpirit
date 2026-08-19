d0950d0e41887398755d52f692ed5bd2a644a1a7
Nickname21223 <qlsl022772@gmail.com>
2026-08-19 13:56:02 +0900

    팔 구역 추가 구현 완료

--- Assets/Scripts/Obstacles/Moss.cs
+++ Assets/Scripts/Obstacles/Moss.cs
@@ -2,19 +2,34 @@ using UnityEngine;
 
 public class MossObstacle : MonoBehaviour, IInteractable
 {
+    [Header("Hidden Object (Optional)")]
+    public GameObject objectToReveal;
+
+    private void Start()
+    {
+        if (objectToReveal != null)
+        {
+            objectToReveal.SetActive(false);
+        }
+    }
+
     public void Interact(GameObject interactor)
     {
-
         if (interactor.TryGetComponent<InventoryManager>(out var inventoryManager))
         {
             if (inventoryManager.TryConsumeItem(ItemType.Scissors, 1))
             {
+                if (objectToReveal != null)
+                {
+                    objectToReveal.SetActive(true);
+                }
+
+                // 이끼 제거
                 Destroy(gameObject);
             }
             else
             {
-
-                Debug.Log("가위 필요");
+                Debug.Log("[가위]가 필요");
             }
         }
     }
