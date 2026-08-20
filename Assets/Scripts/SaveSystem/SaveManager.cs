49697a1bab333d59b7903c57c485182f130c4cf3
김정률 <127955804+rudansrh@users.noreply.github.com>
2026-08-20 21:05:43 +0900

    Revert "패스워드 입력 에러 수정"
    This reverts commit 244ab1d97a9b78077682a6b26e122344fc65ccc7.

--- Assets/Scripts/SaveSystem/SaveManager.cs
+++ Assets/Scripts/SaveSystem/SaveManager.cs
@@ -65,10 +65,9 @@ public class SaveManager : MonoBehaviour
                 }
                 data.isSoulState = playerAbility.isSoul;
             }
-            data.unlockedPassword = PlayerController.Instance.unlockedPassword;
-            PlayerController.Instance.lastSavedSlot = slotIndex;
         }
 
+        PlayerController.Instance.lastSavedSlot = slotIndex;
         string json = JsonUtility.ToJson(data, true);
         File.WriteAllText(GetSaveFilePath(slotIndex), json);
         Debug.Log($"슬롯 {slotIndex}에 게임 저장 완료!");
@@ -144,9 +143,6 @@ public class SaveManager : MonoBehaviour
             }
 
             PlayerController.Instance.lastSavedSlot = lastSlot;
-            PlayerController.Instance.unlockedPassword = currentLoadData.unlockedPassword;
-            PlayerController.Instance.isUIopen = false;
-            PlayerController.Instance.canMove = true;
         }
         currentLoadData = null;
         playerRevive = false;
