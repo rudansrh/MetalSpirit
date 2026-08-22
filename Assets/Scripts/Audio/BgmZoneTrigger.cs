27b2383ceb677c7a764f94a83cd13340bddf6c8d
Yun Junseo <yjs2673@naver.com>
2026-08-23 00:13:16 +0900

    Head&Body에서 빙의 시 BGM 종료 오류 수정

--- Assets/Scripts/Audio/BgmZoneTrigger.cs
+++ Assets/Scripts/Audio/BgmZoneTrigger.cs
@@ -40,10 +40,11 @@ public class BgmZoneTrigger : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (!IsPlayer(other))
-        {
+        if (other.TryGetComponent<PlayerController>(out var player) && player.IsPossessing)
             return;
-        }
+
+        if (!IsPlayer(other))
+            return;
 
         AudioManager.instance?.ExitBgmZone(this);
     }
@@ -55,6 +56,12 @@ public class BgmZoneTrigger : MonoBehaviour
 
     private static bool IsPlayer(Collider2D other)
     {
-        return other.TryGetComponent<PlayerController>(out _);
+        if (other.TryGetComponent<PlayerController>(out _))
+            return true;
+
+        if (other.TryGetComponent<Enemy>(out var enemy) && enemy.isPossessed) // 빙의 중에도 플레이어로 간주
+            return true;
+
+        return false;
     }
 }
