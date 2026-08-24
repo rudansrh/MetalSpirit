14fb7bfb8b57b8821321526bf45c6cdbb4dcbee1
Yun Junseo <yjs2673@naver.com>
2026-08-24 12:14:34 +0900

    Ending Scene 대사 위치 수정, 보스 사망 애니메이션 수정

--- Assets/Scripts/Boss/BossEncounterSequence.cs
+++ Assets/Scripts/Boss/BossEncounterSequence.cs
@@ -448,6 +448,7 @@ public class BossEncounterSequence : MonoBehaviour
             bossAttackController.PlayDeathAnimation();
         }
 
+        AudioManager.instance?.PlaySfx(AudioManager.Sfx.BossDeath); //***
         AudioManager.instance?.PlaySfx(AudioManager.Sfx.BossExplosion); //***
 
         if (bossFadeOutDuration > 0f)
